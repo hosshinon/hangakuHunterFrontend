bc4fae4c7d3107f2235d95d22b327c0c1753efb6
hosshinon <hosshinon@gmail.com>
2024-03-17 07:08:29 +0900

    ciが通るように設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='ja'>
+    <html lang="ja">
       <body className={inter.className}>
         <Header />
         {children}
