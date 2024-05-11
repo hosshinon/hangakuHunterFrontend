a5812ce939e82152b196e80bcf6ee4a7805d5e67
hosshinon <hosshinon@gmail.com>
2024-05-12 07:28:56 +0900

    現在地を取得してマーカー表示する

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,7 +1,9 @@
 'use client'
-import { GoogleMap, LoadScript } from '@react-google-maps/api'
-import React from 'react'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+import React, { useState, useEffect } from 'react'
+import { getCurrentPosition } from '../util/api/Map/getCurrentPosition'
 
+// Google Mapsの表示位置
 const containerStyle = {
   width: '100%',
   height: '50vh',
@@ -9,22 +11,40 @@ const containerStyle = {
     height: '100vh',
   },
 }
-const center = {
+const initialCenter = {
   lat: 35.6804,
   lng: 139.769,
 }
-
 const zoom = 14
 
 const Map = () => {
+  const [currentPosition, setCurrentPosition] = useState(initialCenter)
+
+  useEffect(() => {
+    getCurrentPosition()
+      .then((coords) => {
+        setCurrentPosition(coords)
+      })
+      .catch((error) => {
+        console.error('Error getting location:', error)
+      })
+  }, [])
+
   return (
-    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={zoom}
-      ></GoogleMap>
-    </LoadScript>
+    <>
+      <LoadScript
+        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={currentPosition}
+          zoom={zoom}
+        >
+          {/* 現在位置のマーカー */}
+          {currentPosition.lat && <Marker position={currentPosition} />}
+        </GoogleMap>
+      </LoadScript>
+    </>
   )
 }
 
