41713619e0dfaa0a579e0f66f5c8eb10a27c6336
hosshinon <hosshinon@gmail.com>
2024-05-18 18:13:02 +0900

    feat/マップをクリックした際にピンとサークルを移動する

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,13 +1,21 @@
 /* eslint-disable import/order */
-// Map.js
 'use client'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+  Circle,
+} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
 import markerIcon from 'leaflet/dist/images/marker-icon.png'
 import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+import { useState } from 'react'
 
+// デフォルトのアイコンURLを設定
 delete L.Icon.Default.prototype._getIconUrl
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon.src,
@@ -15,14 +23,34 @@ L.Icon.Default.mergeOptions({
   shadowUrl: markerShadow.src,
 })
 
+// マップのスタイルを設定
 const containerStyle = {
   width: '100%',
   height: '50vh',
 }
 const zoom = 16
-//東京を初期位置に設定
 const InitPosition = [35.681236, 139.767125]
 
+// clickイベントから現在地を取得するコンポーネント
+const LocationMarker = () => {
+  const [position, setPosition] = useState(InitPosition)
+  useMapEvents({
+    click(e) {
+      setPosition(e.latlng)
+    },
+  })
+
+  return position === null ? null : (
+    <div>
+      <Marker position={position}>
+        <Popup>中心はここです</Popup>
+      </Marker>
+      <Circle center={position} radius={500} />
+    </div>
+  )
+}
+
+// マップコンポーネント
 const Map = () => {
   return (
     <div>
@@ -31,11 +59,8 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={InitPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {/* クリックした場所を取得する */}
+        <LocationMarker />
       </MapContainer>
     </div>
   )
